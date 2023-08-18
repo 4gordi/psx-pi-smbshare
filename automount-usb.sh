0754f9cbce503bb1f265750ae3a95ad8f14b7e01
4gordi <4gordi@gmail.com>
2023-08-18 14:30:36 +0300

    Update automount-usb.sh

--- automount-usb.sh
+++ automount-usb.sh
@@ -72,24 +72,52 @@ runuser pi -s /bin/bash -c "/usr/bin/pmount --umask 000 --noatime -w --sync /dev
 #create a new smb share for the mounted drive
 cat <<EOS | sudo tee /etc/samba/smb.conf
 [global]
-server min protocol = NT1
+log level = 1
 workgroup = WORKGROUP
-usershare allow guests = yes
+server string = PS2 Samba Server
+server role = standalone server
+log file = /dev/stdout
+max log size = 0
+load printers = no
+printing = bsd
+printcap name = /etc/printcap
+printcap cache time = 0
+disable spoolss = yes
+pam password change = yes
 map to guest = bad user
-allow insecure wide links = yes
-[ps2smb]
-Comment = Pi default shared folder
-Path = /media/$PART
-Browseable = yes
-Writeable = Yes
-only guest = no
-create mask = 0777
-directory mask = 0777
-Public = yes
-Guest ok = yes
-force user = pi
+usershare allow guests = yes
+create mask = 0664
+force create mode = 0664
+directory mask = 0775
+force directory mode = 0775
+force user =
+force group = users
+server min protocol = NT1
+server signing = disabled
+smb encrypt = disabled
+socket options = TCP_NODELAY TCP_KEEPIDLE=20 IPTOS_LOWDELAY SO_KEEPALIVE
+keepalive = 0
+write cache size = 0
+getwd cache = yes
+large readwrite = yes
+aio read size = 0
+aio write size = 0
+strict locking = no
+strict sync = no
+strict allocate = no
+read raw = no
+write raw = no
 follow symlinks = yes
-wide links = yes
+[ps2smb]
+comment = PlayStation 2
+path = /media/$PART
+browsable = yes
+guest ok = yes
+public = yes
+available = yes
+read only = no
+veto files = /._*/.apdisk/.AppleDouble/.DS_Store/.TemporaryItems/.Trashes/desktop.ini/ehthumbs.db/Network Trash Folder/Temporary Items/Thumbs.db/
+delete veto files = yes
 EOS
 
 #if you wish to create a samba user with password you can use the following:
