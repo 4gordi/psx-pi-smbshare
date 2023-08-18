a4cbd172f2d6fcb05f9e5eb4151f47a2ffa46329
4gordi <4gordi@gmail.com>
2023-08-18 11:25:50 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -33,71 +33,27 @@ sudo apt-get -y install screen wget git curl coreutils
 
 # Install and configure Samba
 sudo apt-get install -y samba samba-common-bin
-wget https://raw.githubusercontent.com/toolboc/psx-pi-smbshare/master/samba-init.sh -O /home/${USER}/samba-init.sh
+wget https://raw.githubusercontent.com/4gordi/psx-pi-smbshare/master/samba-init.sh -O /home/${USER}/samba-init.sh
 chmod 755 /home/${USER}/samba-init.sh
 sudo cp /home/${USER}/samba-init.sh /usr/local/bin
 sudo mkdir -m 1777 /share
 
-# Install ps3netsrv
-sudo rm /usr/local/bin/ps3netsrv++
-sudo apt-get install -y git gcc
-git clone https://github.com/dirkvdb/ps3netsrv--.git
-cd ps3netsrv--
-git submodule update --init
-make CXX=g++
-sudo cp ps3netsrv++ /usr/local/bin
-
 # Install wifi-to-eth route settings
 sudo apt-get install -y dnsmasq
-wget https://raw.githubusercontent.com/toolboc/psx-pi-smbshare/master/wifi-to-eth-route.sh -O /home/${USER}/wifi-to-eth-route.sh
+wget https://raw.githubusercontent.com/4gordi/psx-pi-smbshare/master/wifi-to-eth-route.sh -O /home/${USER}/wifi-to-eth-route.sh
 chmod 755 /home/${USER}/wifi-to-eth-route.sh
 
-# Install setup-wifi-access-point settings
-sudo apt-get install -y hostapd bridge-utils
-wget https://raw.githubusercontent.com/toolboc/psx-pi-smbshare/master/setup-wifi-access-point.sh -O /home/${USER}/setup-wifi-access-point.sh
-chmod 755 /home/${USER}/setup-wifi-access-point.sh
-
-# Remove old XLink Kai Repo if present
-sudo rm -rf /etc/apt/sources.list.d/teamxlink.list
-
-# Set up teamxlink repository and install XLink Kai
-
-sudo apt-get install -y ca-certificates curl gnupg
-sudo mkdir -m 0755 -p /etc/apt/keyrings
-sudo rm /etc/apt/keyrings/teamxlink.gpg
-curl -fsSL https://dist.teamxlink.co.uk/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/teamxlink.gpg
-sudo chmod a+r /etc/apt/keyrings/teamxlink.gpg
-echo  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/teamxlink.gpg] https://dist.teamxlink.co.uk/linux/debian/static/deb/release/ /" | sudo tee /etc/apt/sources.list.d/teamxlink.list > /dev/null
-sudo apt-get update
-sudo apt-get install -y xlinkkai
-
-# Write XLink Kai launch script
-cat <<'EOF' > /home/${USER}/launchkai.sh
-echo "Checking for XLink Kai updates"
-sudo apt-get install xlinkkai -y
-echo "Launching XLink Kai"
-while true; do
-    screen -dmS kai kaiengine
-    sleep 5
-done
-EOF
-
-chmod 755 /home/${USER}/launchkai.sh
-
 # Install USB automount settings
-wget https://raw.githubusercontent.com/toolboc/psx-pi-smbshare/master/automount-usb.sh -O /home/${USER}/automount-usb.sh
+wget https://raw.githubusercontent.com/4gordi/psx-pi-smbshare/master/automount-usb.sh -O /home/${USER}/automount-usb.sh
 chmod 755 /home/${USER}/automount-usb.sh
 sudo /home/${USER}/automount-usb.sh
 
 # Set samba-init + ps3netsrv, wifi-to-eth-route, setup-wifi-access-point, and XLink Kai to run on startup
-{ echo -e "@reboot sudo bash /usr/local/bin/samba-init.sh\n@reboot sudo bash /home/${USER}/wifi-to-eth-route.sh && sudo bash /home/${USER}/setup-wifi-access-point.sh\n@reboot bash /home/${USER}/launchkai.sh"; } | crontab -u pi -
+{ echo -e "@reboot sudo bash /usr/local/bin/samba-init.sh\n@reboot sudo bash /home/${USER}/wifi-to-eth-route.sh"; } | crontab -u pi -
 
 # Start services
 sudo /usr/local/bin/samba-init.sh
 sudo /home/${USER}/wifi-to-eth-route.sh
-sudo /home/${USER}/setup-wifi-access-point.sh
-ps3netsrv++ -d /share/
-screen -dmS kailauncher /home/${USER}/launchkai.sh
 
 # Not a bad idea to reboot
 sudo reboot
